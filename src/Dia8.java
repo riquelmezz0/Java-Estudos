95af139450d177ed0a6018c87bc0a84d00912619
Riquelme <riquelmealyson7@gmail.com>
2026-07-05 16:30:00 -0300

    Dia 8 - Exercícios com while

---
+++ src/Dia8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Dia8 {
+    public static void main(String[] args) {
+
+        int num = 2;
+
+        while (num <= 10) {
+            System.out.println(num);
+            num = num + 2;
+        }
+    }
+}

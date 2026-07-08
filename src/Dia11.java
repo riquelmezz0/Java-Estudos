b0bc9c40750a171fe563de04bcc6beead560ec0a
Riquelme <riquelmealyson7@gmail.com>
2026-07-08 18:23:09 -0300

    Prática da estrutura de repetição for.

---
+++ src/Dia11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dia11 {
+    public static void main(String[] args) {
+
+        for (int num = 1; num <= 10; num++) {
+            System.out.println(num);
+        }
+        for (int num = 2; num <= 20; num = num + 2) {
+            System.out.println(num);
+        }
+        for (int num = 1; num <= 10; num++) {
+            System.out.println("7 x " + num + " = " + num * 7);
+        }
+    }
+}
